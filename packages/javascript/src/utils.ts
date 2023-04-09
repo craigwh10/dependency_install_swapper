17094e758bd504f03afa03a6f44cbb4f4c27a28d
Craig White <craigwh10@gmail.com>
2023-04-09 22:20:20 +0100

    chore(logs): removed in prod builds

--- packages/javascript/src/utils.ts
+++ packages/javascript/src/utils.ts
@@ -1,5 +1,5 @@
 const logger = (scope: 'popup' | 'content') => (level: 'info' | 'warn', message: string) => {
-    console.log(`[${level}] ${scope} - ${message}`);
+    ['test', 'development'].includes(process.env.NODE_ENV as string) ? console.log(`[${level}] ${scope} - ${message}`) : null;
 }
 
 export const popupLogger = logger('popup');
