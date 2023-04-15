ad2e8bdb4952018265be97ce8ac054bef9aca01b
Craig White <craigwh10@gmail.com>
2023-04-15 20:18:55 +0100

    chore(ui): switched ui to use radio for improved ux

--- packages/chrome/src/popup.ts
+++ packages/chrome/src/popup.ts
@@ -2,31 +2,36 @@ import { preferredPackageManager } from './storage'
 import { popupLogger } from './utils'
 
 preferredPackageManager.get().then((res) => {
-  const packageChoiceEl: HTMLSelectElement | null = (
-    document.querySelector('select[name="preferred-package-manager"]')
-  )
+  const packageChoiceEl: NodeListOf<HTMLInputElement> = document.querySelectorAll('input[name="preferred-package-manager"]');
 
   if (packageChoiceEl == null) {
     throw new Error('cannot find preferred package manager select element from popup script')
   }
 
   popupLogger('info', `setting initial ${JSON.stringify(res)}`)
-  packageChoiceEl.value = res.preferredPackageManager
 
-  packageChoiceEl.addEventListener('change', (e): void => {
-    const current = e.target as HTMLSelectElement
-    popupLogger('info', `select changed to ${current.value}`)
-    preferredPackageManager.set(current.value).catch((err) => {
-      popupLogger('warn', err.message)
-    })
-    chrome.tabs.query({ active: true, currentWindow: true }, function (tabs) {
-      if (typeof tabs[0].id === 'number') {
-        chrome.tabs.sendMessage(tabs[0].id, { preferredPackageManager: current.value }, function (response) {
-          popupLogger('info', 'message sent to page content script')
-        })
-      }
-    })
-  }, false)
+  packageChoiceEl.forEach((radioButton: HTMLInputElement) => {
+    if (radioButton.value === res.preferredPackageManager) {
+      radioButton.checked = true;
+    }
+  });
+
+  packageChoiceEl.forEach((choice) => {
+    choice.addEventListener('change', (e): void => {
+      const current = e.target as HTMLInputElement
+      popupLogger('info', `select changed to ${current.value}`)
+      preferredPackageManager.set(current.value).catch((err) => {
+        popupLogger('warn', err.message)
+      })
+      chrome.tabs.query({ active: true, currentWindow: true }, function (tabs) {
+        if (typeof tabs[0].id === 'number') {
+          chrome.tabs.sendMessage(tabs[0].id, { preferredPackageManager: current.value }, function (response) {
+            popupLogger('info', 'message sent to page content script')
+          })
+        }
+      })
+    }, false)
+  })
 }).catch((err) => {
   popupLogger('warn', err.message)
 })
