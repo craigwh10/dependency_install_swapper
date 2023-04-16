0effb36cd4e2ed5acdad560b7d2c05eaec54be2a
Craig White <craigwh10@gmail.com>
2023-04-16 13:31:02 +0100

    chore(ci): increase ws timeout

--- packages/chrome/tests/e2e/getBrowser.ts
+++ packages/chrome/tests/e2e/getBrowser.ts
@@ -3,16 +3,14 @@ import path from 'path';
 
 export const getBrowser = async () => {
     const pathToExtension = path.join(process.cwd(), 'output');
-    // const chromePath = path.join(__dirname, 'chrome.app');
-
     console.log('env', process.env.PUPPETEER_EXEC_PATH);
+
     return puppeteer.launch({
-        headless: process.env.NODE_ENV === "prod" ? false : false,
+        headless: false,
         args: [
         `--disable-extensions-except=${pathToExtension}`,
         `--load-extension=${pathToExtension}`,
         '--disable-features=DialMediaRouteProvider',
-        '--window-size=5000x800',
         process.env.NODE_ENV === 'prod' ? '--no-sandbox' : ''
         ],
         executablePath: process.env.NODE_ENV === 'prod' ? process.env.PUPPETEER_EXEC_PATH : undefined,
